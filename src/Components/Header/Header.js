749a2200019fcf8e5879deee11d0c043f44177c2
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-12 19:34:27 +0100

    header structure created and styling for logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./Header.scss"
+import logoMobile from "../../assets/Logo/InStock-Logo_1x.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <img className='header-logo__img' src = {logoMobile}/>    
+      <nav className='header-nav'>
+        <h3 className='header-nav__warehouses'>Warehouses</h3>
+        <h3 className='header-nav__inventory'>Inventory</h3>
+      </nav>
+    </header>
   )
 }
 
