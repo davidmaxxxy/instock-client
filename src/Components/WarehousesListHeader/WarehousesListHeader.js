c5f86e99331feaad05284d5f9ee1ce1e4ad0e47d
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-14 16:46:25 +0100

    File structer for WarehousesListHeader component

---
+++ src/Components/WarehousesListHeader/WarehousesListHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./WarehousesListHeader.scss";
+
+const WarehousesListHeader = () => {
+    return (
+      <div className="main-heading">
+        
+      </div>
+    );
+  };
+
+  export default WarehousesListHeader;
