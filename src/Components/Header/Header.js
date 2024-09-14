efa65d3b3d6f004bb64f305774226d0e926086c4
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-14 13:03:40 +0100

    move everything in nav tag

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import "./Header.scss"
+import React from "react";
+import "./Header.scss";
 import logoMobile from "../../assets/Logo/InStock-Logo_1x.png";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img className='header-logo__img' src = {logoMobile}/>    
-      <nav className='header-nav'>
-        <h3 className='header-nav__warehouses-btn'>Warehouses</h3>
-        <h3 className='header-nav__inventory-btn'>Inventory</h3>
+    <header className="header">
+      <nav className="header-nav">
+        <img className="header-logo__img" src={logoMobile} />
+        <div className="header-side-nav">
+          <h3 className="header-side-nav__warehouses-btn">Warehouses</h3>
+          <h3 className="header-side-nav__inventory-btn">Inventory</h3>
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
