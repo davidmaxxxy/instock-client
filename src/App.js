f2196eb0ece91de65de7ecdb1423b512ebb99fc3
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-15 01:28:39 +0100

    Header component mobile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
+import "./styles/global.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppNavigations from "./AppNavigations/AppNavigations";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import WarehousesListHeader from "./Components/WarehousesListHeader/WarehousesListHeader";
 
 function App() {
   return (
     <Router>
       <main className="App">
         <Header />
+         
+         <WarehousesListHeader />
          <AppNavigations />
         <Footer />
       </main>
--- src/Components/WarehousesListHeader/WarehousesListHeader.js
+++ src/Components/WarehousesListHeader/WarehousesListHeader.js
@@ -4,7 +4,13 @@ import "./WarehousesListHeader.scss";
 const WarehousesListHeader = () => {
     return (
       <div className="main-heading">
-        
+        <div className="main-heading-container">
+        <h1>Warehouses</h1>
+        <nav className="main-heading-nav">
+          <input className="main-heading-nav__search"/>
+          <button>Placeholder</button>
+        </nav>
+        </div>
       </div>
     );
   };
