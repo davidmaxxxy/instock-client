eb1318132ba027b08298e8561a44abd4cf3727dc
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-09-22 15:30:29 +0100

    feature/SPF2-22 - implemented front end component. Needs to be amended

--- src/AppNavigations/AppNavigations.js
+++ src/AppNavigations/AppNavigations.js
@@ -4,24 +4,35 @@ import DisplayComponents from "../Components/DisplayComponents/DisplayComponents
 import EditWarehouse from "../Components/EditWarehouse/EditWarehouse";
 import WarehouseDetailPage from "../Pages/WarehouseDetailPage/WarehouseDetailPage";
 import InventoryDetailPage from "../Pages/InventoryDetailPage/InventoryDetailPage";
-
+import EditInventoryItem from "../Pages/EditInventoryItem/EditInventoryItem"; // Import the EditInventoryItem component
 
 const AppNavigations = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      {/* to display  components  */}
+
       <Route path="/components" element={<DisplayComponents />} />
+
+      {/* Route for warehouse details */}
       <Route
         path="/warehouse/detail/:warehouseName/:warehouseId"
         element={<WarehouseDetailPage />}
       />
+
+      {/* Route for inventory details */}
       <Route
         path="/inventory/detail/:inventoryName/:inventoryId"
         element={<InventoryDetailPage />}
       />
 
+      {/* Route for editing a warehouse */}
       <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
+
+      {/* Route for editing an inventory item */}
+      <Route
+        path="/inventory/:inventoryId/edit"
+        element={<EditInventoryItem />}
+      />
     </Routes>
   );
 };
