bfa8f8e66ea77267c055b24bdef1b9f4f49e3dc2
Bernard Ayamboro <69229031+bernard493@users.noreply.github.com>
2024-09-22 19:14:05 +0100

    Revert "Revert "Feature spf2 9""

---
+++ src/Components/DropDown/DropDown.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DropDown.scss";
+
+
+const DropDown = ({
+  options,
+  handleOnValueSelect,
+  isError,
+  placeholder,
+
+}) => {
+  return (
+    <div
+      className="select__container"
+    >
+      <select
+        className={`${
+          isError ? "isError" : ""
+        }`} 
+        onChange={(e) => handleOnValueSelect(e.target.value)}
+        defaultValue={placeholder}
+      >
+        <option disabled>{placeholder}</option>
+        {options.map((name, index) => (
+          <option key={index} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default DropDown;
