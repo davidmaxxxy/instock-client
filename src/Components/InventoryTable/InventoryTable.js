5e14d97e306ab48391a9127209761dc9c9f67cdb
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-22 22:59:07 +0100

    InventoryListElment completed

--- src/Components/InventoryTable/InventoryTable.js
+++ src/Components/InventoryTable/InventoryTable.js
@@ -1,44 +1,49 @@
 import React from "react";
 import "./InventoryTable.scss";
 import btnSort from '../../assets/Icons/sort-24px.svg';
+import InventoryElement from "../InventoryElement/InventoryElement";
+import InventoryElementMobile from "../InventoryElementMobile/InventoryElementMobile";
 
-const InventoryTable = ({ inventory, loading }) => { // Destructure props
+const InventoryTable = ({ inventory, loading }) => {
 
   return (
-    <div className="table-wrapper">
+    <div className="inventory-table-wrapper">
       {!loading ? (
-        <section className="table-wrapper__mobile">
-          {/* {inventory.map((item) => (
-            <InventoryElementMobile key={item.id} item={item} /> // Render each inventory item
-          ))} */}
+        <section className="inventory-table-wrapper__mobile">
+          {inventory.map((inventory) => (
+            <InventoryElementMobile key={inventory.id} inventory={inventory} />
+          ))}
         </section>
       ) : (
         <div className="spinner">
           <p>Loading...</p>
         </div>
       )}
-      <table className="table-wrapper__main">
-        <thead className="table-header">
-          <tr className="table-header__cell">
-            <th className="table-header__element"><h4>INVENTORY ITEM</h4> <img className="table-header__btn-sort" src={btnSort} alt="Sort"/> </th> 
+      <table className="inventory-table-wrapper__main">
+        <thead className="inventory-table-header">
+          <tr className="inventory-table-header__cell ">
+            <th className="inventory-table-header__element"><h4>INVENTORY ITEM</h4> <img className="inventory-table-header__btn-sort" src={btnSort} alt="Sort"/> </th> 
           </tr>
-          <tr className="table-header__cell table-header__cell-address">
-            <th className="table-header__element"><h4>CATEGORY</h4> <img className="table-header__btn-sort" src={btnSort} alt="Sort"/></th>
+          <tr className="inventory-table-header__cell inventory-table-header__cell-category">
+            <th className="inventory-table-header__element"><h4>CATEGORY</h4> <img className="inventory-table-header__btn-sort" src={btnSort} alt="Sort"/></th>
           </tr>
-          <tr className="table-header__cell">
-            <th className="table-header__element"><h4>STATUS</h4> <img className="table-header__btn-sort" src={btnSort} alt="Sort"/></th>
+          <tr className="inventory-table-header__cell">
+            <th className="inventory-table-header__element"><h4>STATUS</h4> <img className="inventory-table-header__btn-sort" src={btnSort} alt="Sort"/></th>
           </tr>
-          <tr className="table-header__cell">
-            <th className="table-header__element"><h4>QUANTITY</h4> <img className="table-header__btn-sort" src={btnSort} alt="Sort"/></th>
+          <tr className="inventory-table-header__cell inventory-table-header__cell-qty">
+            <th className="inventory-table-header__element"><h4>QTY</h4> <img className="inventory-table-header__btn-sort" src={btnSort} alt="Sort"/></th>
           </tr>
-          <tr className="table-header__cell table-header__cell-actions">
-            <th className="table-header__element"><h4>ACTIONS</h4></th>
+          <tr className="inventory-table-header__cell">
+            <th className="inventory-table-header__element"><h4>WAREHOUSE</h4> <img className="inventory-table-header__btn-sort" src={btnSort} alt="Sort"/></th>
+          </tr>
+          <tr className="inventory-table-header__cell inventory-table-header__cell-actions">
+            <th className="inventory-table-header__element"><h4>ACTIONS</h4></th>
           </tr>
         </thead>
 
-        {/* {inventory.map((item) => (
-          <InventoryElement key={item.id} item={item} /> // Render each inventory item
-        ))} */}
+        {inventory.map((inventory) => (
+          <InventoryElement key={inventory.id} inventory={inventory} />
+        ))}
       </table>
     </div>
   );
