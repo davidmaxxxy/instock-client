2480661d4227a8381a8156f51aed534da046938c
Bernard Ayamboro <bernardayam493@gmail.com>
2024-09-22 22:58:58 +0100

    added edit inventory

--- src/AppNavigations/AppNavigations.js
+++ src/AppNavigations/AppNavigations.js
@@ -3,30 +3,42 @@ import DisplayComponents from "../Components/DisplayComponents/DisplayComponents
 import EditWarehouse from "../Components/EditWarehouse/EditWarehouse";
 import WarehouseDetailPage from "../Pages/WarehouseDetailPage/WarehouseDetailPage";
 import InventoryDetailPage from "../Pages/InventoryDetailPage/InventoryDetailPage";
+
 import WarehouseListPage from "../Pages/WarehouseListPage/WarehouseListPage";
 import AddNewInventory from "../Pages/AddNewInventory/AddNewInventory";
-import EditInventory from "../Pages/EditInventory/EditInventory";
+
 import InventoryListPage from "../Pages/InventoryListPage/InventoryListPage";
 import WarehouseList from "../Pages/WarehouseListPage/WarehouseListPage";
+import EditInventory from "../Pages/EditInventory/EditInventory";
+
 
 const AppNavigations = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
       {/* to display  components  */}
       <Route path="/components" element={<DisplayComponents />} />
-      <Route
-        path="/warehouse/detail/:warehouseName/:warehouseId"
-        element={<WarehouseDetailPage />}
-      />
       <Route
         path="/inventory/detail/:inventoryName/:inventoryId"
         element={<InventoryDetailPage />}
       />
-
-      <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
+      <Route
+        path="/warehouse/detail/:warehouseName/:warehouseId"
+        element={<WarehouseDetailPage />}
+      />
+      <Route path="/" element={<WarehouseListPage />} />
+      <Route path="/add/inventory/:warehouseId" element={<AddNewInventory />} />
+      <Route path="/edit/inventory/:warehouseId" element={<EditInventory />} />
+      <Route
+        path="/inventory/detail/:inventoryName/:inventoryId"
+        element={<InventoryDetailPage />}
+      />
+      <Route
+        path="/inventory"
+        element={<InventoryListPage />}
+      />
+      <Route path="/warehouse/:id/edit" element={<EditWarehouse />} />
+      <Route path="/warehouse"
+        element={<WarehouseList />}/>
     </Routes>
   );
 };
-
-export default AppNavigations;
